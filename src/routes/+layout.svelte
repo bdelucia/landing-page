1791754aa5431882ba6b3997bf7dc7e2c130c8fe
Bobby DeLucia <bdelucia@asu.edu>
2026-06-04 21:50:17 -0700

    feat: favicon!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import background from '$lib/assets/backgrounds/plane_trail_sky.jpg';
 
 	let { children } = $props();
